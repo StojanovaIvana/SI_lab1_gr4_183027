7669aa3e815853a6cd9cd0774079c7cecb20ac63
Ivana Stojanova <stojanovaivana99@gmail.com>
2020-04-12 00:51:08 +0200

    point class finished

--- Point.java
+++ Point.java
@@ -5,7 +5,12 @@ class Point {
 	//TODO add new variable
 String boja;
 	//TODO constructor
-
+public Point (String id, double x, double y, String boja){
+this.id=id;
+this.x=x;
+this.y=y;
+this.boja=boja;
+ }
 	//TODO setters and getters
 
 	public void move (char xDirection, char yDirection) {
